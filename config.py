4ee63b3e6c6151d16cb3f4e895ac12c3bbcdd6fd
Yan233_ <yan233th@126.com>
2025-12-25 13:45:31 +0800

    Improve global headers

--- config.py
+++ config.py
@@ -5,16 +5,11 @@ headers = {
     "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     "Connection": "keep-alive",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Cache-Control": "no-cache",
     "Origin": "https://jw.shiep.edu.cn",
     "Referer": "https://jw.shiep.edu.cn/eams/stdElectCourse!defaultPage.action",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
     "X-Requested-With": "XMLHttpRequest",
-    "sec-ch-ua": '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
 }
 
 data = {
